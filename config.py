cfac5cfefd313b2aeff7033477f96bbf3e6d17b2
MobtgZhang <3215497340@qq.com>
2022-12-19 19:17:41 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,165 +1,28 @@
 import os
-import logging
-import uuid
+import argparse
 
-from transkg.utils import checkPath
-
-logger = logging.getLogger()
-ROOT_DIR = './data'
-CHECKPOINTS_DIR = './checkpoints'
-def str2bool(v):
-    return v.lower() in ('yes', 'true', 't', '1', 'y')
-def add_args(parser):
-    parser.register('type', 'bool', str2bool)
-
-    # Model architecture
-    model = parser.add_argument_group('Model parameters')
-    model.add_argument('--model-name', type=str, default='TransE',
-                       help='Model architecture type: TransE,TransH,TransR,TransD,TransA,KG2E,NTN,LFM,SME')
-    model.add_argument('--ent-dim', type=int, default=100,
-                       help='Embedding size of entity.')
-    model.add_argument('--rel-dim', type=int, default=100,
-                       help='Embedding size of relation.')
-    # dataset details
-    dataset = parser.add_argument_group('Dataset parameters')
-    dataset.add_argument('--dataset-name', type=str, default="FB15K237",
-                       help='Training the dataset for the model.')
-    dataset.add_argument('--shuffle', type='bool', default=True,
-                         help='Training process whether shuffle the dataset.')
-    # Optimization details
-    optim = parser.add_argument_group('Model Optimization')
-    optim.add_argument('--opt-method', type=str, default='sgd',
-                       help='Optimizer: sgd, adam, adagrad,adadelta')
-    optim.add_argument('--learning-rate', type=float, default=0.01,
-                       help='Learning rate for sgd, adadelta')
-    optim.add_argument('--grad-clipping', type=float, default=10,
-                       help='Gradient clipping')
-    optim.add_argument('--weight-decay', type=float, default=0,
-                       help='Weight decay factor')
-    optim.add_argument('--lr-decay', type=float, default=0.1,
-                       help='learning rate decay factor')
-    optim.add_argument('--momentum', type=float, default=0.1,
-                       help='Momentum factor')
-    optim.add_argument('--rho', type=float, default=0.95,
-                       help='Rho for adadelta')
-    optim.add_argument('--eps', type=float, default=1e-6,
-                       help='Eps for adadelta')
-    # Runtime environment
-    runtime = parser.add_argument_group('Environment')
-    runtime.add_argument('--cuda', type='bool', default=True,
-                         help='Train on CPU, even if GPUs are available.')
-    runtime.add_argument('--gpu', type=int, default=0,
-                         help='Run on a specific GPU')
-    runtime.add_argument('--num-workers', type=int, default=5,
-                         help='Number of subprocesses for data loading')
-    runtime.add_argument('--parallel', type='bool', default=False,
-                         help='Use DataParallel on all available GPUs')
-    runtime.add_argument('--random-seed', type=int, default=1013,
-                         help=('Random seed for all numpy/torch/cuda '
-                               'operations (for reproducibility)'))
-    runtime.add_argument('--num-epoches', type=int, default=10,
-                         help='Train data iterations')
-    runtime.add_argument('--batch-size', type=int, default=16,
-                         help='Batch size for training')
-    runtime.add_argument('--eval-batch-size', type=int, default=16,
-                         help='Batch size for validation/test.')
-    runtime.add_argument('--save-steps', type=int, default=2,
-                         help='Save steps for the training model.')
-    # Files
-    files = parser.add_argument_group('Filesystem')
-    files.add_argument('--root-dir', type=str, default=ROOT_DIR,
-                       help='Directory for dataset.')
-    files.add_argument('--checkpoints-dir', type=str, default=CHECKPOINTS_DIR,
-                       help='Checkpoints files.')
-    files.add_argument('--summary-dir', type=str, default=None,
-                       help='Summary directory.')
-    files.add_argument('--log-file', type=str, default='',
-                       help='Checkpoints files.')
-    files.add_argument('--pre-model', type=str, default=None,
-                       help='Pretraining model for the dataset training.')
-    files.add_argument('--emb-file', type=str, default=None,
-                       help='Embedding files.')
-    # Saving + loading
-    save_load = parser.add_argument_group('Saving/Loading')
-    save_load.add_argument('--checkpoint', type='bool', default=False,
-                           help='Save model + optimizer state after each epoch')
-    # validation
-    validation = parser.add_argument_group('Validation/Test')
-    validation.add_argument('--sim-measure', type=str, default="dot",
-                           help='Evaluation method for the MR,including dot,cos,l1,l2.')
-    validation.add_argument('--eval-method', type=str, default="MR",
-                            help='Evaluation method for the model,including MR and Hit10.')
-def set_default(args):
-    uuid_value = uuid.uuid1()
-    uuid_str = uuid_value.hex
-    args.uuid_str = uuid_str
-    args.log_file = os.path.join(args.checkpoints_dir,args.model_name,args.model_name+"-"+args.uuid_str+".log")
-    args.summary_dir = os.path.join(args.checkpoints_dir,args.model_name,"summary")
-    # model defination
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data-dir",default="./datasets",type=str)
+    parser.add_argument("--result-dir",default="./result",type=str)
+    parser.add_argument("--log-dir",default="./log",type=str)
+    parser.add_argument("--dataset",default="fb15k",type=str)
+    parser.add_argument("--batch-size",default=1024,type=int)
+    parser.add_argument("--emb-dim",default=200,type=int)
+    parser.add_argument("--learning-rate",default=0.01,type=float)
+    parser.add_argument("--epoches",default=20,type=int)
+    parser.add_argument("--cuda",action="store_false")
+    args = parser.parse_args()
+    return args
 def check_args(args):
-    assert (args.model_name in ["TransE","TransH","TransR","TransD","TransA","KG2E","NTN","LFM","SME"])
-    assert (args.dataset_name in ["FB15K237"])
-    TransE = {"emb_dim": 100,
-              "margin": 1.0,
-              "L": 2}
-    TransH = {"emb_dim": 100,
-              "margin": 1.0,
-              "L": 2,
-              "C": 0.01,
-              "eps": 0.001}
-    TransD = {"ent_dim": 100,
-              "rel_dim": 100,
-              "margin": 2.0,
-              "L": 2}
-    TransA = {"emb_dim": 100,
-              "margin": 3.2,
-              "L": 2,
-              "lamb": 0.01,
-              "C": 0.2}
-    TransR = {"emb_dim": 100,
-              "margin": 1.0,
-              "L": 2, }
-    KG2E = {"emb_dim": 100,
-            "margin": 4.0,
-            "sim": "EL",
-            "vmin": 0.03,
-            "vmax": 3.0}
-    SME = {"ent_dim": 100,
-           "rel_dim": 100,
-           "L": 2,
-           "ele_dot": True}
-    NTN = {"ent_dim": 100,
-           "rel_dim": 100,
-           "bias_flag": True,
-           "rel_flag": True,
-           "margin": 1.0}
-    LFM = {
+    dataset_dir = os.path.join(args.data_dir,args.dataset)
+    result_dir = os.path.join(args.result_dir,args.dataset)
+    log_dir = os.path.join(args.log_dir,args.dataset)
+    assert os.path.exists(dataset_dir)
+    assert args.dataset in ["fb15k","fb15k-237","kg20c","wn18","wn18rr","yago3-10"]
+    if not os.path.exists(result_dir):
+        os.makedirs(result_dir)
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
 
-    }
-    if args.model_name == "TransE":
-        args.model_kargs = TransE
-    elif args.model_name == "TransH":
-        args.model_kargs = TransH
-    elif args.model_name == "TransR":
-        args.model_kargs = TransR
-    elif args.model_name == "TransD":
-        args.model_kargs = TransD
-    elif args.model_name == "TransA":
-        args.model_kargs = TransA
-    elif args.model_name == "KG2E":
-        args.model_kargs = KG2E
-    elif args.model_name == "NTN":
-        args.model_kargs = NTN
-    elif args.model_name == "LFM":
-        args.model_kargs = LFM
-    elif args.model_name == "SME":
-        args.model_kargs = SME
-    else:
-        logger.error("No model named %s" % (args.model_name))
-        exit(1)
-def check_paths(args):
-    checkPath(args.root_dir,raise_error=False)
-    checkPath(args.checkpoints_dir,raise_error=False)
-    checkPath(os.path.join(args.checkpoints_dir, args.model_name),raise_error=False)
-    checkPath(os.path.join(args.root_dir, args.dataset_name, "processed"), raise_error=False)
-    checkPath(os.path.join(args.root_dir, args.dataset_name, "raw"), raise_error=False)
+    
--- main.py
+++ main.py
@@ -1,71 +1,100 @@
-import sys
-import argparse
+import os
+import time
 import logging
+logger = logging.getLogger()
 
 import numpy as np
 import torch
-from transkg.dataset.FB15K237 import FB15K237Trainer,preparingFB15273Dataset,FB15K237Tester
-from tensorboardX import SummaryWriter
-from config import add_args,set_default,check_paths,check_args
+from torch.utils.data import DataLoader
 
-logger = logging.getLogger()
+import config
+from transkg import create_ents_rels,create_pos_neg_ids
+from transkg import Dictionary,EntityrelationDataset
+from transkg import TransE
 
 def main(args):
-    # preparing dataset
-    sumWriter = SummaryWriter(log_dir=args.summary_dir)
-    preparingFB15273Dataset(args.root_dir)
-    trainer = FB15K237Trainer(args)
-    trainer.prepareData()
-    trainer.prepareModel(args.model_kargs)
-    if args.pre_model:
-        trainer.loadPretrainModel(args.pre_model)
-    if args.emb_file:
-        trainer.loadPretrainEmbedding(args.emb_file)
-    trainer.run()
-    trainer.save()
-    saved_model_file = trainer.model_file
-    del trainer
-    tester = FB15K237Tester(args)
-    tester.set_model(saved_model_file)
-    tester.prepareData()
-    tester.run_link_prediction()
-    sumWriter.close()
-if __name__ == '__main__':
-    # Parse cmdline args and setup environment
-    parser = argparse.ArgumentParser(
-        'TransKG framework for training embeddings.',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
-    add_args(parser)
-    args = parser.parse_args()
-    set_default(args)
-    check_args(args)
-    check_paths(args)
-    # Set cuda
-    args.use_gpu = args.cuda and torch.cuda.is_available()
-    if args.cuda:
-        torch.cuda.set_device(args.gpu)
-
-    # Set random state
-    np.random.seed(args.random_seed)
-    torch.manual_seed(args.random_seed)
-    if args.cuda:
-        torch.cuda.manual_seed(args.random_seed)
-
-    # Set logging
-    logger.setLevel(logging.INFO)
-    fmt = logging.Formatter('%(asctime)s: [ %(message)s ]',
-                            '%m/%d/%Y %I:%M:%S %p')
-    console = logging.StreamHandler()
-    console.setFormatter(fmt)
-    logger.addHandler(console)
-    if args.log_file:
-        if args.checkpoint:
-            logfile = logging.FileHandler(args.log_file, 'a')
+    # generate the entities and relations
+    data_dir = os.path.join(args.data_dir,args.dataset)
+    result_dir = os.path.join(args.result_dir,args.dataset)
+    # saved entity and relations files
+    load_ent_file = os.path.join(result_dir,"entity2id.txt")
+    load_rel_file = os.path.join(result_dir,"relation2id.txt")
+    # raw train ,valid and test dataset
+    tags_list = ["train","valid","test"]
+    if not os.path.exists(load_ent_file) or not os.path.exists(load_rel_file):
+        files_list = [os.path.join(data_dir,"%s.txt"%tag) for tag in tags_list]
+        create_ents_rels(files_list,load_ent_file,load_rel_file)
+        logger.info("The entity and relation saved in directory %s."%result_dir)
+    else:
+        logger.info("The files has already saved in directory %s"%result_dir)
+    # load dictionary
+    ent_dict= Dictionary.load(load_ent_file)
+    rel_dict= Dictionary.load(load_rel_file)
+    for tag in tags_list:
+        save_filename = os.path.join(result_dir,"%s_ids.txt"%tag)
+        if not os.path.exists(save_filename):
+            load_filename = os.path.join(data_dir,"%s.txt"%tag)
+            create_pos_neg_ids(load_filename,result_dir,ent_dict,rel_dict,tag,True if tag=="train" else False)
+            logger.info("The entity and relation saved in file %s."%save_filename)
         else:
-            logfile = logging.FileHandler(args.log_file, 'w')
-        logfile.setFormatter(fmt)
-        logger.addHandler(logfile)
-    logger.info('COMMAND: %s' % ' '.join(sys.argv))
-    print(args)
+            logger.info("The files has already saved in file %s"%save_filename)
+    
+    device = torch.device("cuda" if args.cuda and torch.cuda.is_available() else "cpu")
+    train_file = os.path.join(result_dir,"train_ids.txt")
+    train_neg_file = os.path.join(result_dir,"train_neg_ids.txt")
+    train_dataset = EntityrelationDataset(train_file,train_neg_file)
+    train_loader = DataLoader(train_dataset,batch_size= args.batch_size,shuffle=True)
+    valid_file = os.path.join(result_dir,"valid_ids.txt")
+    valid_dataset = EntityrelationDataset(valid_file)
+    valid_loader = DataLoader(valid_dataset,batch_size= args.batch_size,shuffle=False)
+    test_file = os.path.join(result_dir,"test_ids.txt")
+    test_dataset = EntityrelationDataset(test_file)
+    test_loader = DataLoader(test_dataset,batch_size= args.batch_size,shuffle=False)
+    
+    # create models
+    ent_size = len(ent_dict)
+    rel_size = len(rel_dict)
+    model = TransE(ent_size,rel_size,args.emb_dim)
+    model.to(device)
+    optimizer = torch.optim.SGD(model.parameters(),lr=args.learning_rate,)
+    for epoch in range(args.epoches):
+        avg_loss = 0.0
+        for item in train_loader:
+            optimizer.zero_grad()
+            pos_x,neg_x = item
+            pos_x = pos_x.to(device)
+            neg_x = neg_x.to(device)
+            loss = model(pos_x,neg_x)
+            avg_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+        avg_loss /= len(train_loader)
+        print(avg_loss)
+        # test the dataset 
+if __name__ == "__main__":
+    # get and check argumentation
+    args = config.get_args()
+    config.check_args(args)
+    # First step, create a logger
+    logger = logging.getLogger()
+    # The log level switch
+    logger.setLevel(logging.INFO)
+    # Second step, create a handler,which is used to write to logging file.
+    args.time_step_str = time.strftime("%Y%m%d%H%M",time.localtime(time.time()))
+    log_file = os.path.join(args.log_dir,args.time_step_str+".log")
+    fh = logging.FileHandler(log_file, mode='w')
+    # The log's switch of the output log file
+    fh.setLevel(logging.DEBUG)
+    # Third, define the output formatter
+    format_str = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+    fh.setFormatter(format_str)
+    ch = logging.StreamHandler()
+    ch.setFormatter(format_str)
+    # Fourth, add the logger into handler
+    logger.addHandler(fh)
+    logger.addHandler(ch)
     main(args)
+
+
+
+
